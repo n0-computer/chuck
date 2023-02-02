218390c271bb217e4606123ed9a77f42da6da1b0
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2023-02-02 14:20:23 +0100

    ci?

--- netsim/setup.sh
+++ netsim/setup.sh
@@ -1,10 +1,15 @@
 sudo apt install mininet
 sudo apt install openvswitch-testcontroller
 sudo apt install iperf
-mkdir logs
-mkdir report
-mkdir -p /var/lib/netsim
-sudo chmod -R 777 /var/lib/netsim
-cd /var/lib/netsim
-dd if=/dev/urandom of=128MB.bin bs=64M count=2 iflag=fullblock
-dd if=/dev/urandom of=1GB.bin bs=64M count=16 iflag=fullblock
+mkdir -p logs
+mkdir -p report
+mkdir -p data
+cd data
+rm -f 1G.bin
+rm -f 100M.bin
+for i in {1..10}; do
+    cat ../../fixtures/10MiB.car >> 100M.bin
+done
+for i in {1..10}; do
+    cat 100M.bin >> 1G.bin
+done
