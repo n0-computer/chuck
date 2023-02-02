590566f8f40cd0dd76d23794f9b14374186fb384
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2023-02-02 15:16:39 +0100

    drop iroh deps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use anyhow::{Context, Result};
-use chuck::{iroh, memesync};
+use anyhow::{Result};
 use clap::{Parser, Subcommand};
 
 use axum::{
@@ -26,19 +25,6 @@ struct Args {
 #[derive(Debug, Subcommand)]
 enum Commands {
     #[clap(arg_required_else_help = true)]
-    MemesyncReceiver { port: u16, sleep: u8 },
-    #[clap(arg_required_else_help = true)]
-    MemesyncSender {
-        target_host: String,
-        bytes: u64,
-        filename: String,
-        sleep: u8,
-    },
-    #[clap(arg_required_else_help = true)]
-    IrohServer { target_host: String },
-    #[clap(arg_required_else_help = false)]
-    IrohClient {},
-    #[clap(arg_required_else_help = true)]
     HttpServer { tls: u8 },
     #[clap(arg_required_else_help = false)]
     HttpClient { target_host: String },
@@ -50,34 +36,6 @@ enum Commands {
 async fn main() -> Result<()> {
     let args = Args::parse();
     match args.command {
-        Commands::MemesyncReceiver { port, sleep } => {
-            let mut receiver = memesync::MemesyncReceiver::new(port).await;
-            receiver.run(sleep).await?;
-        }
-        Commands::MemesyncSender {
-            target_host,
-            bytes,
-            filename,
-            sleep,
-        } => {
-            let mut sender = memesync::MemesyncSender::new(target_host).await;
-            sender
-                .send_file(bytes, filename, sleep)
-                .await
-                .context("failed to send file")?;
-        }
-        Commands::IrohServer { target_host } => {
-            let node = iroh::Node::new(4444, true).await;
-            iroh::run_server(&node, target_host)
-                .await
-                .context("failed to run server")?;
-        }
-        Commands::IrohClient {} => {
-            let node = iroh::Node::new(4454, false).await;
-            iroh::run_client(&node)
-                .await
-                .context("failed to run client")?;
-        }
         Commands::HttpServer { tls } => {
             let addr = SocketAddr::from(([0, 0, 0, 0], 443));
             let serve_dir =
