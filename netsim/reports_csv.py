7d0b9e9072a8ff31336adf73e0c0914076bb524e
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2023-02-23 23:35:37 +0100

    feat: table format output for netsim comments (#4)
    * feat: table format output for netsim comments
    
    * improvements
    
    * format as table
    
    * sort output

--- netsim/reports_csv.py
+++ netsim/reports_csv.py
@@ -10,11 +10,31 @@ def res_to_prom(res, commit):
                 labels += ',commit="%s"' % commit
             print('throughput{%s} %f' % (labels, t))
 
+case_order = ['1_to_1', '1_to_3', '1_to_5', '1_to_10', '2_to_2', '2_to_4', '2_to_6', '2_to_10']
+
+def case_sort(x):
+    if x[0] in case_order:
+        return case_order.index(x[0])
+    else:
+        case_order.append(x[0])
+        return len(case_order) - 1
+
+def res_to_table(res):
+    
+    print('| test | case | throughput_gbps |')
+    print('| ---- | ---- | ---------- |')
+    for k, v in res.items():
+        vl = [(g,h) for g,h in v.items()]
+        vl = sorted(vl, key=case_sort)
+        for c, t in vl:
+            print('| %s | %s | %.2f |' % (k, c, t))
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--commit", help = "commit hash")
     parser.add_argument("--prom", help = "generate output for prometheus", action='store_true')
+    parser.add_argument("--table", help = "generate output for github comments", action='store_true')
     args = parser.parse_args()
 
     files = []
@@ -53,5 +73,7 @@ if __name__ == '__main__':
 
     if args.prom:
         res_to_prom(res, args.commit)
+    elif args.table:
+        res_to_table(res)
     else:
         print(json.dumps(res, indent=4, sort_keys=True))
