4567065803180086690696f7a4dd10503ccff0eb
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2023-08-07 11:06:14 +0200

    export RUST_BACKTRACE

--- netsim/main.py
+++ netsim/main.py
@@ -59,6 +59,7 @@ def run(nodes, prefix, args, debug=False, full_debug=False, visualize=False):
         # reduce logging, track only those of interest
         # magicsock::endpoint required for iroh integration tests
         env_vars['RUST_LOG'] = 'error,iroh_net::hp::magicsock::endpoint=debug'
+        env_vars['RUST_BACKTRACE'] = '1'
         if full_debug:
             env_vars['RUST_LOG'] = 'debug'
 
