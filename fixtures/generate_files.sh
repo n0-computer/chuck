b018e27e586c67a906b65a193898552b8c03c628
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2024-08-26 14:52:07 +0200

    feat: test many files (#63)
    * feat: test many files
    
    * fix
    
    * fix
    
    * improve reporting
    
    * cleanup

---
+++ fixtures/generate_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <number_of_files> <file_size_in_bytes> <output_folder>"
+    exit 1
+fi
+
+# Assign arguments to variables
+NUMBER_OF_FILES=$1
+FILE_SIZE=$2
+OUTPUT_FOLDER=$3
+
+# Create the output folder if it doesn't exist
+mkdir -p "$OUTPUT_FOLDER"
+
+# Generate a random 4-character string
+RANDOM_STRING=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
+
+# Generate the files
+for (( i=1; i<=$NUMBER_OF_FILES; i++ ))
+do
+    # Generate a file with the specified size
+    dd if=/dev/urandom of="$OUTPUT_FOLDER/file_${RANDOM_STRING}_$i.bin" bs=$FILE_SIZE count=1
+done
+
+echo "Generated $NUMBER_OF_FILES files of size $FILE_SIZE bytes in $OUTPUT_FOLDER with prefix file_$RANDOM_STRING"
